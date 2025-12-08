05172a3ab56559f8818f661174f1674e1eebfdb4
CesarOrt <lopezortigozacesarramon@gmail.com>
2025-12-07 21:53:01 -0600

    Es una redireccion a la ventana de 'Bienvenido' (index.php)

---
+++ logout_redirect.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * ARCHIVO NUEVO: logout_redirect.php
+ * 
+ * PROPÓSITO: Cerrar la sesión del usuario y redirigirlo al login.
+ * Este archivo se llamará desde el botón de "Cerrar sesión" en el frontend.
+ * 
+ * UBICACIÓN: Colocar en la raíz del proyecto junto a logout.php
+ * Ruta: C:\xampp\htdocs\Proyecto_Final\logout_redirect.php
+ */
+
+// Iniciamos la sesión para poder destruirla
+session_start();
+
+// Destruimos toda la sesión
+session_destroy();
+
+// Redirigimos al login
+header("Location: /Proyecto_final/Backend");
+exit;
+?>
